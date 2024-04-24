162c4f8d1d375b3dcd9bfe722d4de48e513e4653
Jiwon Yoon <jiwon105020@gmail.com>
2024-04-24 21:03:24 +0900

    update demoUI.py

--- CapUI/demoUI.py
+++ CapUI/demoUI.py
@@ -1,16 +1,17 @@
-import tkinter as tk
-import numpy as np
-import MousePainter as mp
-from utils import rdp
+import MousePainter as MP
 import argparse
 
 parser = argparse.ArgumentParser()
 # Evaluation input filepaths
-parser.add_argument('--savefile_name', type=str, help='set output file name', default='mouse_deltas.npy')
+parser.add_argument('--save_file_name', type=str, help='set output file name for original input',
+                    default='mouse_deltas.npy')
+parser.add_argument('--rdp_file_name', type=str, help='set output file name for rdp', default='rdp_deltas.npy')
+parser.add_argument('--ai_file_name', type=str, help='set output file name for AI drawing', default='ai_deltas.npy')
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    painter = mp.MousePainter(args)
+    painter = MP.MousePainter(args)
     painter.run()
+
 
 
